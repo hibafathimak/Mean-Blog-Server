d0d5d4375bc78cade89ccbaf0c2ed31cb63dc785
hibaaahhh <hibakalathil02@gmail.com>
2025-05-21 16:34:20 +0530

    Blog Server

--- middlewares/multerMiddleware.js
+++ middlewares/multerMiddleware.js
@@ -2,7 +2,7 @@ const multer = require('multer')
 
 const storage = multer.diskStorage({
     destination:(req,file,callback)=>{
-        callback(null,"./uploads")  
+        callback(null,"../uploads")  
     },
     filename:(req,file,callback)=>{
         callback(null,`image-${Date.now()}-${file.originalname}`) 
