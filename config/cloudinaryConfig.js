71f0be4d924e79ae18a5229ccab9ddf4150892e2
hibaaahhh <hibakalathil02@gmail.com>
2025-05-22 16:00:12 +0530

    blog server

--- config/cloudinaryConfig.js
+++ config/cloudinaryConfig.js
@@ -2,9 +2,9 @@ const cloudinary = require('cloudinary').v2;
 require('dotenv').config();
 
 cloudinary.config({
-  cloud_name: process.env.CLND_NAME,
-  api_key: process.env.CLND_API_KEY,
-  api_secret: process.env.CLND_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 
